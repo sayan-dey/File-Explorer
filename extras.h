b9eacc0cf0b36d84b83cc1a3e0abc4cdffedfe48
Sayan Dey <70585239+sayan-dey@users.noreply.github.com>
2021-01-21 12:01:18 +0530

    contains headers

---
+++ extras.h
@@ -0,0 +1,26 @@
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+
+//#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <dirent.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+
+//#include <cstdio>
+//#include <cstdlib>
+#include <sstream>         // stringstream
+#include <fstream>
+#include <list>
+#include <stack>
+#include <string>
+#include <string.h>
+#include <ftw.h>
+#include <limits>
+#include <cstdint>
+#include <iostream>
+
+#define INT_MAX (std::numeric_limits<std::int32_t>::max())
+
+#endif
